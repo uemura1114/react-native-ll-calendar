4e12a5663a6f583a3b6dacf61bb48214ae2e7718
Wataru Uemura <bbwtsn.par72@gmail.com>
2025-09-23 15:36:11 +0900

    chore: change babel config

--- babel.config.js
+++ babel.config.js
@@ -9,4 +9,14 @@ module.exports = {
       presets: ['module:@react-native/babel-preset'],
     },
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
