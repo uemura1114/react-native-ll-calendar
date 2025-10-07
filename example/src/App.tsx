37f185499f46afc970621d97e05d4ff7899fcabf
Wataru Uemura <bbwtsn.par72@gmail.com>
2025-10-07 19:31:50 +0900

    chore: add example events

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import { useCallback, useMemo, useState } from 'react';
 import { Alert } from 'react-native';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { MonthCalendar, type CalendarEvent } from 'react-native-ll-calendar';
 
 export default function App() {
@@ -163,6 +163,186 @@ export default function App() {
         borderColor: '#e17055',
         color: '#0e0e0e',
       },
+      {
+        id: '14',
+        title: 'Event 1',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#ff6b6b',
+        borderColor: '#e55353',
+        color: '#0e0e0e',
+      },
+      {
+        id: '15',
+        title: 'Event 2',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#4ecdc4',
+        borderColor: '#45b7aa',
+        color: '#0e0e0e',
+      },
+      {
+        id: '16',
+        title: 'Event 3',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#45b7d1',
+        borderColor: '#3a9bc1',
+        color: '#0e0e0e',
+      },
+      {
+        id: '17',
+        title: 'Event 4',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#96ceb4',
+        borderColor: '#7fb069',
+        color: '#0e0e0e',
+      },
+      {
+        id: '18',
+        title: 'Event 5',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#ffeaa7',
+        borderColor: '#fdcb6e',
+        color: '#0e0e0e',
+      },
+      {
+        id: '19',
+        title: 'Event 6',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#dda0dd',
+        borderColor: '#ba68c8',
+        color: '#0e0e0e',
+      },
+      {
+        id: '20',
+        title: 'Event 7',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#74b9ff',
+        borderColor: '#0984e3',
+        color: '#0e0e0e',
+      },
+      {
+        id: '21',
+        title: 'Event 8',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#fd79a8',
+        borderColor: '#e84393',
+        color: '#0e0e0e',
+      },
+      {
+        id: '22',
+        title: 'Event 9',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#fdcb6e',
+        borderColor: '#e17055',
+        color: '#0e0e0e',
+      },
+      {
+        id: '23',
+        title: 'Event 10',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#a29bfe',
+        borderColor: '#6c5ce7',
+        color: '#0e0e0e',
+      },
+      {
+        id: '24',
+        title: 'Event 11',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#fd79a8',
+        borderColor: '#e84393',
+        color: '#0e0e0e',
+      },
+      {
+        id: '25',
+        title: 'Event 12',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#00b894',
+        borderColor: '#00a085',
+        color: '#0e0e0e',
+      },
+      {
+        id: '26',
+        title: 'Event 13',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#fab1a0',
+        borderColor: '#e17055',
+        color: '#0e0e0e',
+      },
+      {
+        id: '27',
+        title: 'Event 14',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#ff6b6b',
+        borderColor: '#e55353',
+        color: '#0e0e0e',
+      },
+      {
+        id: '28',
+        title: 'Event 15',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#4ecdc4',
+        borderColor: '#45b7aa',
+        color: '#0e0e0e',
+      },
+      {
+        id: '29',
+        title: 'Event 16',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#45b7d1',
+        borderColor: '#3a9bc1',
+        color: '#0e0e0e',
+      },
+      {
+        id: '30',
+        title: 'Event 17',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#96ceb4',
+        borderColor: '#7fb069',
+        color: '#0e0e0e',
+      },
+      {
+        id: '31',
+        title: 'Event 18',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#ffeaa7',
+        borderColor: '#fdcb6e',
+        color: '#0e0e0e',
+      },
+      {
+        id: '32',
+        title: 'Event 19',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#dda0dd',
+        borderColor: '#ba68c8',
+        color: '#0e0e0e',
+      },
+      {
+        id: '33',
+        title: 'Event 20',
+        start: new Date(currentYear, 9, 8),
+        end: new Date(currentYear, 9, 8),
+        backgroundColor: '#74b9ff',
+        borderColor: '#0984e3',
+        color: '#0e0e0e',
+      },
     ];
   }, []);
 
@@ -177,16 +357,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <MonthCalendar
-          defaultDate={date}
-          weekStartsOn={1}
-          onChangeDate={handleChangeDate}
-          events={events}
-          onPressEvent={handleEventPress}
-          onPressCell={handleCellPress}
-        />
-      </ScrollView>
+      <MonthCalendar
+        defaultDate={date}
+        weekStartsOn={1}
+        onChangeDate={handleChangeDate}
+        events={events}
+        onPressEvent={handleEventPress}
+        onPressCell={handleCellPress}
+      />
     </View>
   );
 }
@@ -194,8 +372,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollView: {
     marginVertical: 80,
     width: '100%',
   },
